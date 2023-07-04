d4629f09343bccec45119a22cd0944dafdc48a53
Achindra2003 <138213900+Achindra2003@users.noreply.github.com>
2023-07-04 23:50:21 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+function toggleNav() {
+    document.querySelector(".outer-grid").classList.toggle("outer-grid-expanded");
+    document.querySelector("nav").classList.toggle("hide-nav");
+  }
+  
+  function likealert() {
+    alert('😊');
+  }
+  
+  function dislikealert() {
+    alert('😡');
+  }
+  
+  function giftalert() {
+    alert('🖕');
+  } 
